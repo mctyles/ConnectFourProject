8ec57ee0bf1dc01978283b7032ee2d6abad9f73c
tylermcmu <94275794+tylermcmu@users.noreply.github.com>
2022-07-23 10:18:45 -0700

    updates to app.js

--- app.js
+++ app.js
@@ -1,51 +1,64 @@
 const playerSelectMenu = document.getElementById('player-select-menu');
 const playerSelectButton = document.getElementById('player-select-button');
 let playerOptionSelected = 'singleplayer';
+let playerNameButton = '';
+const playerNames = document.getElementById('player-names');
 
 playerSelectMenu.addEventListener('change', function(event){
     playerOptionSelected = event.target.value;
 })
 
+
 function enterPlayerName() {
-    const playerNames = document.getElementById('player-names');
     const gametypeSelect = document.getElementById('gametype-select');
     gametypeSelect.remove();
     if (playerOptionSelected === 'singleplayer') {
-        let playerNameText = document.createElement('p');
-        let playerNameInput = document.createElement('input');
-        let playerNameButton = document.createElement('button');
-        playerNameText.className = 'player-name-text';
-        playerNameInput.className = 'player-name-input';
+        let playerNameText1 = document.createElement('p');
+        let playerNameInput1 = document.createElement('input');
+        playerNameButton = document.createElement('button');
+        playerNameText1.className = 'player-name-text';
+        playerNameInput1.className = 'player-name-input';
         playerNameButton.className = 'player-name-button';
-        playerNameText.innerText = "Enter your name here:";
+        playerNameText1.innerText = "Enter your name here:";
         playerNameButton.innerText = "Start";
-        playerNames.appendChild(playerNameText);
-        playerNames.appendChild(playerNameInput);
+        playerNames.appendChild(playerNameText1);
+        playerNames.appendChild(playerNameInput1);
         playerNames.appendChild(playerNameButton);
     } else {
         let playerNameText1 = document.createElement('p');
         let playerNameInput1 = document.createElement('input');
-        let playerNameButton1 = document.createElement('button');
         let playerNameText2 = document.createElement('p');
         let playerNameInput2 = document.createElement('input');
-        let playerNameButton2 = document.createElement('button');
+        playerNameButton = document.createElement('button');
         playerNameText1.className = 'player-name-text-1';
         playerNameInput1.className = 'player-name-input-1';
-        playerNameButton1.className = 'player-name-button-1';
         playerNameText2.className = 'player-name-text-2';
         playerNameInput2.className = 'player-name-input-2';
-        playerNameButton2.className = 'player-name-button-2';
-        playerNameText1.innerText = "Enter Player 1 name here:";
-        playerNameButton1.innerText = "Enter";
-        playerNameText2.innerText = "Enter Player 2 name here:";
-        playerNameButton2.innerText = "Enter";
+        playerNameButton.className = 'player-name-button';
+        playerNameText1.innerText = "Enter Player 1 name:";
+        playerNameText2.innerText = "Enter Player 2 name:";
+        playerNameButton.innerText = "Start";
         playerNames.appendChild(playerNameText1);
         playerNames.appendChild(playerNameInput1);
-        playerNames.appendChild(playerNameButton1);
         playerNames.appendChild(playerNameText2);
         playerNames.appendChild(playerNameInput2);
-        playerNames.appendChild(playerNameButton2);
+        playerNames.appendChild(playerNameButton);
     }
 }
 
 playerSelectButton.addEventListener('click', enterPlayerName);
+
+function startGame() {
+    let player1Name = playerNameInput1.value;
+    let player2Name = playerNameInput2.value;
+    let playerList = document.createElement('h3');
+    if (!playerNameInput2) {
+        player2Name = 'Computer';
+    } 
+    playerList.innerText = `Player 1: ${player1Name} vs Player 2: ${player2Name}`;
+    playerNames.appendChild(playerList);
+}
+
+playerNameButton.addEventListener('click', startGame);
+
+let gameState = {};
