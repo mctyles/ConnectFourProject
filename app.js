328a5375a458bd6b0d7dede92cabdae7116c9f5c
tylermcmu <94275794+tylermcmu@users.noreply.github.com>
2022-07-28 19:19:17 -0700

    changes to app.js, index.html, style.css

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-const playerSelectMenu = document.getElementById('player-select-menu');
-const playerSelectButton = document.getElementById('player-select-button');
-let playerOptionSelected = 'singleplayer';
-const playerNames = document.getElementById('player-names');
-const turnStatus = document.getElementById('turn-status');
-
-
-let gameState = {};
+let state = {};
 
 function initializeGame (){
-    gameState.game = [
+    state.game = [
         //           0  1  2  3  4  5  6
                     [0, 0, 0, 0, 0, 0, 0], //0
                     [0, 0, 0, 0, 0, 0, 0], //1
@@ -16,27 +9,87 @@ function initializeGame (){
                     [0, 0, 0, 0, 0, 0, 0], //3
                     [0, 0, 0, 0, 0, 0, 0], //4
                     [0, 0, 0, 0, 0, 0, 0]  //5
-            ]
-    gameState.indexLastPlay = [];
-    gameState.player1turn = true;
-    gameState.playerNumber = 0;
-    gameState.tileColor = '';
-    gameState.oneplayer = true;
-    gameState.currentPlayerName = '';
-    gameState.win = false;
+            ];
+    state.player1turn = true;
+    state.playerNumber = 0;
+    state.tileColor = '';
+    state.oneplayer = true;
+    state.currentPlayerName = '';
+    state.win = false;
+    state.rowNumPlayed = 0;
+    state.colNumPlayed = 0;
 }
+
+const playerSelectMenu = document.getElementById('player-select-menu');
+const playerSelectButton = document.getElementById('player-select-button');
+const playerNames = document.getElementById('player-names');
+const turnStatus = document.getElementById('turn-status');
+let playerOptionSelected = 'singleplayer';
 
 playerSelectMenu.addEventListener('change', function(event){
     playerOptionSelected = event.target.value;
 })
 
+function enterPlayerName() {
+    initializeGame();
+    const gametypeSelect = document.getElementById('gametype-select');
+    gametypeSelect.remove();
+    let playerNameContainer = document.createElement('div');
+    let playerNameText1 = document.createElement('p');
+    let playerNameInput1 = document.createElement('input');
+    let playerNameButton = document.createElement('button');
+    playerNameContainer.className = 'player-name-container';
+    playerNameText1.className = 'player-name-text-1';
+    playerNameInput1.className = 'player-name-input-1';
+    playerNameButton.className = 'player-name-button';
+    playerNameText1.innerText = "Enter your name:";
+    playerNameButton.innerText = "Start";
+    playerNames.appendChild(playerNameContainer);
+    playerNameContainer.appendChild(playerNameText1);
+    playerNameContainer.appendChild(playerNameInput1);
+        if (playerOptionSelected === 'twoplayers') {
+        state.oneplayer = false;
+        let playerNameText2 = document.createElement('p');
+        let playerNameInput2 = document.createElement('input');
+        playerNameText2.className = 'player-name-text-2';
+        playerNameText1.innerText = "Enter Player 1 name:";
+        playerNameInput2.className = 'player-name-input-2';
+        playerNameText2.innerText = "Enter Player 2 name:";
+        playerNameContainer.appendChild(playerNameText2);
+        playerNameContainer.appendChild(playerNameInput2);
+    }
+        playerNameContainer.appendChild(playerNameButton);
+        playerNameButton.addEventListener('click', startGame);
+}
+
+playerSelectButton.addEventListener('click', enterPlayerName);
+
 const gameContainer = document.getElementById('game');
 const boardTable = document.createElement('table');
 
+function startGame(){
+    let playerNameInput1 = document.querySelector('.player-name-input-1');
+    let playerNameInput2 = document.querySelector('.player-name-input-2');
+    let playerNameContainer = document.querySelector('.player-name-container');
+    state.player1Name = playerNameInput1.value;
+    state.player2Name = '';
+    if (playerOptionSelected === 'twoplayers') {
+        state.player2Name = playerNameInput2.value;
+    }
+    let playerList = document.createElement('h3');
+    if (!playerNameInput2){
+        state.player2Name = 'Computer';
+    }
+    playerList.innerText = `Player 1: ${state.player1Name} vs Player 2: ${state.player2Name}`;
+    playerNames.appendChild(playerList);
+    playerNameContainer.remove();
+    setBoard();
+}
+
 function checkSlots(columnNum) {
     for (let i = 5; i >= 0; i--) {
-        if (gameState.game[i][columnNum] === 0){
-            gameState.game[i][columnNum] = 'mark';
+        if (state.game[i][columnNum] === 0){
+            state.game[i][columnNum] = 'mark';
             break;
         }
     }
@@ -47,23 +100,27 @@ function getRandomColumnNum() {
 }
 
 function determineTurn (){
-    if (gameState.player1turn){
-        gameState.playerNumber = 1;
-        gameState.tileColor = 'yellow';
+    if (state.player1turn){
+        state.playerNumber = 1;
+        state.tileColor = 'yellow';
     } else {
-        gameState.playerNumber = 2;
-        gameState.tileColor = 'red';
+        state.playerNumber = 2;
+        state.tileColor = 'red';
     }
-    gameState.indexLastPlay = [];  
+    state.lastPlayCoordinates = [];  
 }
 
 function updateTurnStatusText() {
-    if (gameState.player1turn) {
-        gameState.currentPlayerName = `${gameState.player1Name}`;
+    if (state.player1turn) {
+        state.currentPlayerName = `${state.player1Name}`;
+        turnStatus.style.color = 'yellow';
+        turnStatus.style.textShadow = '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black';
     } else {
-        gameState.currentPlayerName = `${gameState.player2Name}`
+        state.currentPlayerName = `${state.player2Name}`
+        turnStatus.style.color = 'red';
+        turnStatus.style.textShadow = '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black';       
     }
-    turnStatus.innerText = `It's ${gameState.currentPlayerName}'s turn!`
+    turnStatus.innerText = `It's ${state.currentPlayerName}'s turn!`
 }
 
 function setBoard() {
@@ -104,208 +161,179 @@ function setBoard() {
     gameContainer.appendChild(boardTable);
     
     updateTurnStatusText();
+    
     playArrowContainer.addEventListener('click', function(event){ 
-        if (event.target.className === 'play-arrow'){
+        if (event.target.className === 'play-arrow' && !state.win){
             let currentOpenSlot = '';
             checkSlots(Number(event.target.id));
             determineTurn();
             for (let i = 5; i >= 0; i--) {
-                if (gameState.game[i][Number(event.target.id)] === 'mark') {
+                if (state.game[i][Number(event.target.id)] === 'mark') {
                     currentOpenSlot = document.getElementById(`row-${i}-slot-${Number(event.target.id)}`)
-                    currentOpenSlot.style.backgroundColor = `${gameState.tileColor}`;
-                    gameState.game[i][Number(event.target.id)] = gameState.playerNumber;
-                    gameState.indexLastPlay.push(i);
-                    gameState.indexLastPlay.push(Number(event.target.id));
-                    gameState.player1turn = gameState.player1turn !== true;
+                    currentOpenSlot.style.backgroundColor = `${state.tileColor}`;
+                    state.game[i][Number(event.target.id)] = state.playerNumber;
+                    state.rowNumPlayed = i;
+                    state.colNumPlayed = Number(event.target.id);
+                    state.player1turn = state.player1turn !== true;
                     checkForWin();
+                    if (!state.win) {
+                        updateTurnStatusText();
+                    }
                 }
             }
-            updateTurnStatusText();
-            if (gameState.oneplayer) {
+            if (state.oneplayer && !state.win) {
                 determineTurn();
                 let randomColumnNum = getRandomColumnNum();
                 checkSlots(randomColumnNum);
                 for (let i = 5; i >= 0; i--) {
-                    if (gameState.game[i][randomColumnNum] === 'mark'){
+                    if (state.game[i][randomColumnNum] === 'mark'){
                         currentOpenSlot = document.getElementById(`row-${i}-slot-${randomColumnNum}`)
-                        currentOpenSlot.style.backgroundColor = `${gameState.tileColor}`;
-                        gameState.game[i][randomColumnNum] = gameState.playerNumber;
-                        gameState.indexLastPlay.push(i);
-                        gameState.indexLastPlay.push(Number(event.target.id));
-                        gameState.player1turn = gameState.player1turn !== true;
+                        currentOpenSlot.style.backgroundColor = `${state.tileColor}`;
+                        state.game[i][randomColumnNum] = state.playerNumber;
+                        state.rowNumPlayed = i;
+                        state.colNumPlayed = Number(event.target.id);
+                        state.player1turn = state.player1turn !== true;
                         checkForWin();
+                        if (!state.win) {
+                            updateTurnStatusText();
+                        }
+                        console.log('test')
                     }
                 }
-                updateTurnStatusText();
             }
         }
-        checkForWin();
     });
 }
 
-function checkRowForWin(){
-    let rowNumPlayed = gameState.indexLastPlay[0];
+function checkRowForWin(row, column){
     let tileMatchCount = 0;
-    for (let i = 0; i < 7; i++) {
-        if (gameState.game[rowNumPlayed][i] === undefined) {
-            break;
+    for (let i = 1; i < 4; i++) {
+        if (typeof state.game[row][column + i] === 'undefined') {
+            continue;
         }
-        if (gameState.game[rowNumPlayed][i] !== 0 && gameState.game[rowNumPlayed][i] === gameState.game[rowNumPlayed][i - 1]) {
+        if (state.game[row][column] === state.game[row][column + i]) {
             tileMatchCount++;
         } else {
-            tileMatchCount = 0;
+            break;
         }
     }
-    if (tileMatchCount >= 4) {
-        gameState.win = true;
+    for (let i = 1; i < 4; i++) {
+        if (typeof state.game[row][column - i] === 'undefined') {
+            continue;
+        }
+        if (state.game[row][column] === state.game[row][column - i]) {
+            tileMatchCount++;
+        } else {
+            break;
+        }
     }
+    if (tileMatchCount >= 3) {
+        state.win = true;
+    }
+    console.log(tileMatchCount);
 }
 
-function checkColumnForWin(){
-    let colNumPlayed = gameState.indexLastPlay[1];
+function checkColumnForWin(row, column){
     let tileMatchCount = 0;
-    for (let i = 0; i < 7; i++) {
-        if (gameState.game[i][colNumPlayed] === undefined){
-            break;
-        }
-        if (gameState.game[i][colNumPlayed] !== 0 && gameState.game[i][colNumPlayed] === gameState.game[i - 1][colNumPlayed]){
+    for (let i = row; i < row + 4; i++) {
+        if (state.game[i] && state.playerNumber === state.game[i][column]) {
             tileMatchCount++;
         } else {
-            tileMatchCount = 0;
+            break;
         }
     }
     if (tileMatchCount >= 4) {
-        gameState.win = true;
+        state.win = true;
     }
+    console.log(tileMatchCount);
 }
 
-
-function checkPositiveDiagForWin() {
-    let rowNumPlayed = gameState.indexLastPlay[0];
-    let ColNumPlayed = gameState.indexLastPlay[1];
+function checkPositiveDiagForWin(row, column) {
     let tileMatchCount = 0; 
-    let newTile = gameState.game[rowNumPlayed][ColNumPlayed];
-    for (let i = 0; i < 6; i ++){
-        newTile = gameState.game[rowNumPlayed++][ColNumPlayed--];
-        rowNumPlayed++;
-        ColNumPlayed--;
-        if (newTile === undefined) {
-            newTile = gameState.game[rowNumPlayed - 1][ColNumPlayed + 1];
-            rowNumPlayed--;
-            ColNumPlayed++;
-            break;
+    for (let i = 1; i < 4; i++) {
+        if (state.game[row - i] && typeof state.game[row - i][column + i] === 'undefined') {
+            continue;
         }
-    }
-    for (let i = 0; i < 6; i ++) {
-        newTile = gameState.game[rowNumPlayed--][ColNumPlayed++];
-        if (newTile === undefined) {
-            break;
-        }
-        if (newTile !== 0 && newTile === gameState.game[rowNumPlayed + 1][ColNumPlayed - 1]){
+        if (state.game[row - i] && state.game[row][column] === state.game[row - i][column + i]) {
             tileMatchCount++;
         } else {
-            tileMatchCount = 0;
+            break;
         }
     }
-    if (tileMatchCount >= 4) {
-        gameState.win = true;
+    for (let i = 1; i < 4; i++) {
+        if (state.game[row + i] && typeof state.game[row + i][column - i] === 'undefined') {
+            continue;
+        }
+        if (state.game[row + i] && state.game[row][column] === state.game[row + i][column - i]) {
+            tileMatchCount++;
+        } else {
+            break;
+        }
     }
+    if (tileMatchCount >= 3) {
+        state.win = true;
+    }
+    console.log(tileMatchCount);
 }
 
-function checkNegativeDiagForWin() {
-    let rowNumPlayed = gameState.indexLastPlay[0];
-    let ColNumPlayed = gameState.indexLastPlay[1];
+function checkNegativeDiagForWin(row, column) {
     let tileMatchCount = 0; 
-    let newTile = gameState.game[rowNumPlayed][ColNumPlayed];
-    for (let i = 0; i < 6; i ++){
-        newTile = gameState.game[rowNumPlayed--][ColNumPlayed++];
-        rowNumPlayed--;
-        ColNumPlayed++;
-        if (newTile === undefined) {
-            newTile = gameState.game[rowNumPlayed + 1][ColNumPlayed - 1];
-            rowNumPlayed++;
-            ColNumPlayed--;
-            break;
+    for (let i = 1; i < 4; i++) {
+        if (state.game[row - i] && typeof state.game[row - i][column - i] === 'undefined') {
+            continue;
         }
-    }
-    for (let i = 0; i < 6; i ++) {
-        newTile = gameState.game[rowNumPlayed++][ColNumPlayed--];
-        if (newTile === undefined) {
-            break;
-        }
-        if (newTile !== 0 && newTile === gameState.game[rowNumPlayed - 1][ColNumPlayed + 1]){
+        if (state.game[row - i] && state.game[row][column] === state.game[row - i][column - i]) {
             tileMatchCount++;
         } else {
-            tileMatchCount = 0;
+            break;
         }
     }
-    if (tileMatchCount >= 4) {
-        gameState.win = true;
+    for (let i = 1; i < 4; i++) {
+        if (state.game[row + i] && typeof state.game[row + i][column + i] === 'undefined') {
+            continue;
+        }
+        if (state.game[row + i] && state.game[row][column] === state.game[row + i][column + i]) {
+            tileMatchCount++;
+        } else {
+            break;
+        }
     }
+    if (tileMatchCount >= 3) {
+        state.win = true;
+    }
+    console.log(tileMatchCount);
 }
 
 function checkForWin(){
-    checkRowForWin();
-    checkColumnForWin();
-    checkPositiveDiagForWin();
-    checkNegativeDiagForWin();
-    if (gameState.win) {
-        turnStatus.innerText = `${gameState.currentPlayerName} Wins!`
+    let rowNum = state.rowNumPlayed;
+    let colNum = state.colNumPlayed;
+    checkRowForWin(rowNum, colNum);
+    checkColumnForWin(rowNum, colNum);
+    checkPositiveDiagForWin(rowNum, colNum);
+    checkNegativeDiagForWin(rowNum, colNum);
+    if (state.win) {
+        turnStatus.innerText = `${state.currentPlayerName} Wins!`
     }
 }
 
-function startGame(){
-    let playerNameInput1 = document.querySelector('.player-name-input-1');
-    let playerNameInput2 = document.querySelector('.player-name-input-2');
-    let playerNameContainer = document.querySelector('.player-name-container');
-    gameState.player1Name = playerNameInput1.value;
-    gameState.player2Name = '';
-    if (playerOptionSelected === 'twoplayers') {
-        gameState.player2Name = playerNameInput2.value;
+/* for (let i = 0; i < 6; i++){
+    checkSlots(i);
+    for (let j = 5; j >= 0; j--) {
+        if (state.game[j][i] === 'mark') {
+            state.game[j][i] = 2;
+            checkForWin();
+            if (!state.win) {
+                state.game[j][i] = 1;
+                checkForWin();
+                if (state.win){
+                    state.game[j][i] = 2;
+                    currentOpenSlot = document.getElementById(`row-${j}-slot-${i}`)
+                    currentOpenSlot.style.backgroundColor = `${state.tileColor}`;
+                } else {
+
+                }
+            }
+        }
     }
-    let playerList = document.createElement('h3');
-    if (!playerNameInput2){
-        gameState.player2Name = 'Computer';
-    }
-    playerList.innerText = `Player 1: ${gameState.player1Name} vs Player 2: ${gameState.player2Name}`;
-    playerNames.appendChild(playerList);
-    playerNameContainer.remove();
-    setBoard();
 }
-
-function enterPlayerName() {
-    initializeGame();
-    const gametypeSelect = document.getElementById('gametype-select');
-    gametypeSelect.remove();
-    let playerNameContainer = document.createElement('div');
-    let playerNameText1 = document.createElement('p');
-    let playerNameInput1 = document.createElement('input');
-    let playerNameButton = document.createElement('button');
-    playerNameContainer.className = 'player-name-container';
-    playerNameText1.className = 'player-name-text-1';
-    playerNameInput1.className = 'player-name-input-1';
-    playerNameButton.className = 'player-name-button';
-    playerNameText1.innerText = "Enter your name:";
-    playerNameButton.innerText = "Start";
-    playerNames.appendChild(playerNameContainer);
-    playerNameContainer.appendChild(playerNameText1);
-    playerNameContainer.appendChild(playerNameInput1);
-        if (playerOptionSelected === 'twoplayers') {
-        gameState.oneplayer = false;
-        let playerNameText2 = document.createElement('p');
-        let playerNameInput2 = document.createElement('input');
-        playerNameText2.className = 'player-name-text-2';
-        playerNameText1.innerText = "Enter Player 1 name:";
-        playerNameInput2.className = 'player-name-input-2';
-        playerNameText2.innerText = "Enter Player 2 name:";
-        playerNameContainer.appendChild(playerNameText2);
-        playerNameContainer.appendChild(playerNameInput2);
-    }
-        playerNameContainer.appendChild(playerNameButton);
-        playerNameButton.addEventListener('click', startGame);
-}
-
-playerSelectButton.addEventListener('click', enterPlayerName);
-
-//let playerNameButton = document.querySelector('.player-name-button');
-//playerNameButton.addEventListener('click', startGame);
+*/
